b827820091e2a72e9fb7bc6eebfcf9f1e524c82d
angelonoda <angelonoda@gmail.com>
2021-10-14 20:44:41 -0300

    Setando tamanho do papel e orientação

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -131,6 +131,13 @@ class TarefaController extends Controller
     {
         $tarefas = auth()->user()->tarefas()->get();
         $pdf = PDF::loadView('tarefa.pdf', ['tarefas' => $tarefas]);
-        return $pdf->download('lista_de_tarefas.pdf');
+
+        $pdf->setPaper('a4', 'landscape');
+        //tipo de papel: a4, letter
+        //orientação: landscape (paisagem), portrait (retrato)
+
+
+        //return $pdf->download('lista_de_tarefas.pdf');
+        return $pdf->stream('lista_de_tarefas.pdf');
     }
 }
